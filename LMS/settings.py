0c9c8f1edd9e43a9c984b39debc62a043cd1c18c
Aditya-1406 <gargaditya674@gmail.com>
2026-02-16 20:53:43 +0530

    added requirements.txt

--- LMS/settings.py
+++ LMS/settings.py
@@ -292,5 +292,5 @@ EMAIL_USE_TLS = True
 
 
 
-EMAIL_HOST_USER = 'YOUR EMAIL @gmail.com'
-EMAIL_HOST_PASSWORD = 'YOUR APP PASSWORD'  # NOT normal password
+# EMAIL_HOST_USER = 'YOUR EMAIL @gmail.com'
+# EMAIL_HOST_PASSWORD = 'YOUR APP PASSWORD'  # NOT normal password
